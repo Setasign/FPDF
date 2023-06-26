0838e0ee4925716fcbbc50ad9e1799b5edfae0a0
Jan Slabon <jan.slabon@setasign.com>
2023-06-26 16:44:25 +0200

    Update to 1.86

--- tutorial/makefont.php
+++ tutorial/makefont.php
@@ -2,5 +2,5 @@
 // Generation of font definition file for tutorial 7
 require('../makefont/makefont.php');
 
-MakeFont('calligra.ttf','cp1252');
+MakeFont('CevicheOne-Regular.ttf', 'cp1252');
 ?>
--- tutorial/tuto7.php
+++ tutorial/tuto7.php
@@ -1,11 +1,10 @@
 <?php
-define('FPDF_FONTPATH','.');
 require('../fpdf.php');
 
 $pdf = new FPDF();
-$pdf->AddFont('CevicheOne','','CevicheOne-Regular.php');
+$pdf->AddFont('CevicheOne','','CevicheOne-Regular.php','.');
 $pdf->AddPage();
 $pdf->SetFont('CevicheOne','',45);
-$pdf->Cell(0,10,'Enjoy new fonts with FPDF!');
+$pdf->Write(10,'Enjoy new fonts with FPDF!');
 $pdf->Output();
 ?>
